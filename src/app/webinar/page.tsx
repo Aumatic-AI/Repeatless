2de05b9e401f4fa337000c4f13b453f5772ea7b6
nishthapnt <nishthapnt@gmail.com>
2026-09-18 10:58:38 +0530

    add pixel tracking to webinar page

--- src/app/webinar/page.tsx
+++ src/app/webinar/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import WebinarHero from "./components/WebinarHero";
 import ProblemSection from "./components/ProblemSection";
 import StorySection from "./components/StorySection";
@@ -18,16 +20,37 @@ export const metadata: Metadata = {
 
 export default function WebinarPage() {
   return (
-    <main className="bg-paper">
-      <WebinarHero />
-      <ProblemSection />
-      <StorySection />
-      <SkillSection />
-      <DiscoverSection />
-      <AttendSection />
-      <PricingReasonSection />
-      <FAQSection />
-      <FinalCTASection />
-    </main>
+    <>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '1976376849844362');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <main className="bg-paper">
+        <WebinarHero />
+        <ProblemSection />
+        <StorySection />
+        <SkillSection />
+        <DiscoverSection />
+        <AttendSection />
+        <PricingReasonSection />
+        <FAQSection />
+        <FinalCTASection />
+      </main>
+    </>
   );
 }
